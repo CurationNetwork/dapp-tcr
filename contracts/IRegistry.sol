8b8c52b3313b34d44d77271e42b10ce50e7cd0e8
Eenae <z-line@my.com>
2018-10-26 23:44:06 +0200

    contract interface: improvements and state machine clarification

--- contracts/IRegistry.sol
+++ contracts/IRegistry.sol
@@ -3,13 +3,26 @@ pragma solidity 0.4.25;
 
 interface IRegistry {
 
+    // states: NOT_EXISTS (0); APPLICATION (1); EXISTS (2); EDIT (3); DELETING (4)
+
+    // transitions:
+    // NOT_EXISTS -> APPLICATION
+    // APPLICATION -> EXISTS | NOT_EXISTS
+    // EXISTS -> EDIT | DELETING | NOT_EXISTS
+    // EDIT -> EXISTS
+    // DELETING -> NOT_EXISTS
+
+
     // ADDING & EDITING
 
     // add new dapp to the registry
-    function apply(bytes ipfs_hash) public;
+    function apply(bytes ipfs_hash) public; // STATES: NOT_EXISTS
 
     // edit existing dapp meta info
-    function edit(bytes32 _listingHash, bytes new_ipfs_hash) public;
+    function edit(bytes32 _listingHash, bytes new_ipfs_hash) public;    // STATES: EXISTS
+
+    // remove dapp added by you
+    function init_exit(bytes32 _listingHash) public; // STATES: EXISTS
 
 
     // VIEW
@@ -18,32 +31,32 @@ interface IRegistry {
     function list() public view returns (bytes32[] ids);
 
     function get_info(bytes32 _listingHash) public view returns
-        (bool exists, bool is_application, bool is_challenged /* application and edit can be challenged */,
-        bool is_editing, bool is_exiting,
+        (uint state, bool is_challenged /* many states can be challenged */,
         bool status_can_be_updated /* if updateStatus should be called */,
         bytes ipfs_hash, bytes edit_ipfs_hash /* empty if not editing */);
 
-    // status of a challenge
-    function challenge_status(bytes32 _listingHash) public view returns
-        (uint challenge_id, bool is_commit, bool is_reveal, uint votesFor /* 0 for commit phase */,
-        uint votesAgainst /* 0 for commit phase */);
 
+    // MAINTENANCE
 
-    // remove dapp added by you
-    function initExit(bytes32 _listingHash) public;
+    function canUpdateStatus(bytes32 _listingHash) public view returns (bool);
 
     // finish current operation
-    function updateStatus(bytes32 _listingHash) public;
+    function updateStatus(bytes32 _listingHash) public; // only if canUpdateStatus
 
 
     // CHALLENGES
 
     // open challenge for a dapp
-    function challenge(bytes32 _listingHash) public;
+    function challenge(bytes32 _listingHash, uint state_check /* to prevent race condition */) public;    // STATES:
 
-    function commitVote(bytes32 _listingHash, bytes32 _secretHash) public;
+    // status of a challenge
+    function challenge_status(bytes32 _listingHash) public view returns
+        (uint challenge_id, bool is_commit, bool is_reveal, uint votesFor /* 0 for commit phase */,
+        uint votesAgainst /* 0 for commit phase */);    // only for challenged
 
-    function revealVote(bytes32 _listingHash, uint _voteOption /* 1: for, other: against */, uint _voteStake, uint _salt) public;
+    function commitVote(bytes32 _listingHash, bytes32 _secretHash) public;  // only for challenged
+
+    function revealVote(bytes32 _listingHash, uint _voteOption /* 1: for, other: against */, uint _voteStake, uint _salt) public;  // only for challenged
 
     // claim challenge reward
     function claimReward(uint challenge_id) public;
