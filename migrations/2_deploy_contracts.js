5bb9fc8353ad6d43a71b2aa89cc5f7368cfd0e6b
Eenae <z-line@my.com>
2018-10-27 19:20:57 +0200

    migration

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Token = artifacts.require("./Token.sol");
+const Voting = artifacts.require("./Voting.sol");
+const Parameterizer = artifacts.require("./Parameterizer.sol");
+const Registry = artifacts.require("Registry.sol");
+
+
+module.exports = function(deployer, network) {
+  let token;
+  let voting;
+  let params;
+
+  deployer.deploy(Token).then(function (ret) {
+    token = ret;
+    return deployer.deploy(Voting);
+  }).then(function (ret) {
+    voting = ret;
+    return deployer.deploy(Parameterizer, [web3.toWei(1, 'ether'), 60, 60, 60, 50, 51, 60, 60]);
+  }).then(function(ret){
+    params = ret;
+    return deployer.deploy(Registry, token.address, voting.address, params.address);
+  }).then(function(registry){
+    return voting.set_registry(registry.address);
+  });
+};
