6d890b26628ec4917aee2ee77b4c0dba4658eaa2
Boogerwooger <sergey@mixbytes.io>
2018-10-27 09:54:48 +0000

    merge modular schemas into json schema form

--- app/components/forms/ItemForm.js
+++ app/components/forms/ItemForm.js
@@ -2,8 +2,33 @@ import React from 'react';
 import Form from "react-jsonschema-form";
 import { spec } from './spec.js';
 
+const main_schema = require('../../../schema/main-spec-v0.1.0.json');
+
+let module_schemas = {};
+module_schemas['contact'] = require('../../../schema/module-contact-spec-v0.1.0.json');
+module_schemas['ontology'] = require('../../../schema/module-ontology-spec-v0.1.0.json');
+module_schemas['standard'] = require('../../../schema/module-standard-spec-v0.1.0.json');
+
 class ItemForm extends React.Component {
+  
   render() {
+	// MERGING SCHEMAS
+	for (module in module_schemas) {
+		for (let k in module_schemas[module]) {
+			if (k == 'definitions') {
+				for (let kk in module_schemas[module]['definitions']) {
+					if (kk in main_schema['definitions']) {
+						console.log("Error, key '" + k + "' already exists in main_schema.definitions");
+						continue;
+					}
+					main_schema['definitions'][kk] = module_schemas[module]['definitions'][kk];
+				}
+				delete module_schemas[module]['definitions'];
+			}
+			main_schema['properties'][module] = module_schemas[module];
+		}
+	}
+
     const onSubmit = ({formData}) => console.log("Data submitted: ",  formData);
 
     return (<div>
@@ -11,8 +36,8 @@ class ItemForm extends React.Component {
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
       {/* END Do not touch this*/}
-
-      <Form schema={spec} onSubmit={onSubmit} />
+	
+      <Form schema={main_schema} onSubmit={onSubmit} />
     </div>);
   }
 }
