3b7f6bcad209bb7f77ad7e2a3d35d5ccfbc794f3
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2018-10-27 11:20:36 +0300

    Sandbox for form

--- app/components/Dapp.js
+++ app/components/Dapp.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
 
 import BlockSubmitted from './blocks/BlockSubmitted';
 import BlockChallengedRemove from './blocks/BlockChallengedRemove';
 import BlockChallengedUpdate from './blocks/BlockChallengedUpdate';
-import BlockRegistry from './blocks/BlockRegistry';
+// import BlockRegistry from './blocks/BlockRegistry';
+import ItemForm from './forms/ItemForm';
 
 import './Dapp.scss';
 
-class Dapp extends React.Component {
-  render() {
+function Main ({location}) {
+  
+  if (/form=1/.test(location.search)) {
+    return (<ItemForm/>);
+
+  } else {
     return (<div className="dapp-container">
       <BlockSubmitted/>
       <BlockChallengedUpdate/>
       <BlockChallengedRemove/>
       {/* 
-      <BlockRegistry/>
+        <BlockRegistry/>
       */}
     </div>);
+  }
+}
+
+const browserHistory = createBrowserHistory();
+
+class Dapp extends React.Component {
+  render() {
+    return (<Router history={browserHistory}>
+      <Switch>
+        <Route path="/" component={Main}/>
+      </Switch>
+    </Router>);
   }
 }
 
--- app/components/forms/ItemForm.js
+++ app/components/forms/ItemForm.js
@@ -7,6 +7,11 @@ class ItemForm extends React.Component {
     const onSubmit = ({formData}) => console.log("Data submitted: ",  formData);
 
     return (<div>
+      {/* Do not touch this*/}
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+      {/* END Do not touch this*/}
+
       <Form schema={spec} onSubmit={onSubmit} />
     </div>);
   }
