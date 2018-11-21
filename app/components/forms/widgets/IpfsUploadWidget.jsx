0b34d1035762a83ecc4c3624059e14f74da2ea02
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2018-11-21 19:12:18 +0300

    IPFS file upload fixed, accidently updated packages and switched to yarn

--- app/components/forms/widgets/IpfsUploadWidget.jsx
+++ app/components/forms/widgets/IpfsUploadWidget.jsx
@@ -1,33 +1,17 @@
-import React, { Component, PureComponent } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { uploadFileToIpfs } from 'helpers/ipfs-tools';
 
 import './IpfsUploadWidget.scss';
 
-export default class IPFSUploadWebGatewayWidget extends PureComponent {
+export default class IPFSUploadWebGatewayWidget extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
       msg: props.value || 'Choose file',
-      url: null,
-      uploadEndpoint: 'https://ipfs.smartz.io',
-    }
+    };
 
     this.onChange = this.onChange.bind(this);
-  }
-
-  readFile(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new window.FileReader();
-      reader.onload = event => resolve(event.target.result);
-      reader.onerror = event => reject(event.target.error);
-      reader.onloadstart = event => this.setState({ msg: 'Reading...' });
-      reader.readAsText(file);
-    });
-  }
-
-  uploadIpfs(data) {
-    return axios.post(this.state.uploadEndpoint + '/ipfs/', data);
   }
 
   onChange(event) {
@@ -35,20 +19,20 @@ export default class IPFSUploadWebGatewayWidget extends PureComponent {
     const file = event.target.files[0];
 
     if (!file) {
-      this.setState({ msg: null }, onChange(null));
+      this.setState({ msg: 'Choose file' }, onChange(null));
     } else {
-      this.readFile(file)
-        .then(data => {
-          this.data = data;
-          this.setState({ msg: 'Uploading to IPFS...' });
-          return this.uploadIpfs(data)
-        })
-        .then(resp => {
-          const hash = resp.headers['ipfs-hash'];
+      this.setState({ msg: 'Uploading file to IPFS...' });
+
+      uploadFileToIpfs(file)
+        .then((hash) => {
           this.setState({
             msg: hash,
             ipfs_hash: hash
           }, onChange(this.state.ipfs_hash));
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({ msg: 'Error :-('});
         });
     }
   };
@@ -72,8 +56,6 @@ export default class IPFSUploadWebGatewayWidget extends PureComponent {
           className={(msg === 'Choose file') ? '' : 'active'}>
           {msg}
         </label>
-
-        {this.state.url ? (<p>{this.state.url}</p>) : null}
       </div>
     );
   }
--- app/helpers/ipfs-tools.js
+++ app/helpers/ipfs-tools.js
@@ -1,7 +1,20 @@
+import fileReader  from 'pull-file-reader';
+import ipfsAPI from 'ipfs-api';
+
 export const isIpfsHash = (hash) => {
   if (typeof hash === 'string') {
     return /^Qm[1-9A-HJ-NP-Za-km-z]{44}$/.test(hash);
   } else {
     return false;
   }
+};
+
+export const uploadFileToIpfs = (file) => {
+  const ipfsApi = ipfsAPI({host: 'ipfs.twitch666.mixbytes.io', port: '5001', protocol: 'https'});
+
+  return new Promise((resolve, reject) => {
+    ipfsApi.add(fileReader(file))
+    .then((response) => resolve(response[0].hash))
+    .catch((error) => reject(error));
+  });
 };
